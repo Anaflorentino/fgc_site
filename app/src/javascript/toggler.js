68af10341f7c4326e948795f0deaa6f12f7f5857
Ana Florentino <abmflorentino@gmail.com>
2024-12-01 19:57:14 -0300

    Keeping up to date

--- app/src/javascript/toggler.js
+++ app/src/javascript/toggler.js
@@ -1,5 +1,7 @@
-const toggle = document.getElementById('pet-friendly-toggle');
-        toggle.addEventListener('change', () => {
-            const message = toggle.checked ? "Active: Pet Friendly" : "Disabled: Pet Friendly";
-            console.log(message);
-        });
+const switcher = document.getElementById("switcher");
+const textLabel = document.querySelector(".switch-text");
+
+// Alternar o estado ao clicar no texto ou no switcher
+textLabel.addEventListener("click", () => {
+  switcher.checked = !switcher.checked;
+});
